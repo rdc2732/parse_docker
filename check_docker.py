ad902f326ebf5d0d15d80e20ec57fafa6491daa4
Rick Cottle <richard.cottle@honeywell.com>
2018-10-04 15:32:05 -0700

    Added time reporting of status check.

--- check_docker.py
+++ check_docker.py
@@ -4,24 +4,32 @@
 #   needed to verify the ContextSDM processes are running.
 
 import sys
-from subprocess import Popen, PIPE, STDOUT, call
+import os
+from datetime import datetime
+
+status_file_name = "/home/nagiosrm/docker_status.txt"
 
 if len(sys.argv) != 2:
     exit('One container not specified')
 else:
     container = sys.argv[1]
 
-# proc=Popen('docker ps', shell=True, stdout=PIPE,)
-# output=proc.communicate()[0]
-# lines = output.split('\n')
+try:
+    status_file = open(status_file_name)
+except IOError:
+    exit (status_file_name + " does not exist.")
 
-# 1) Get output of 'docker ps' command, which is returned as a string.  For testing use docker_status.txt
+status_time = os.path.getmtime("docker_status.txt")
+status_time_str = str(datetime.fromtimestamp(status_time))
+status_time_message = "Docker Process Status: " + status_time_str
 
-# 2) Break output into lines
-text_file = open('docker_status.txt', 'r')
-lines = text_file.readlines()
+if container == "STATUS":
+    print status_time_message
+    exit(0)
 
-# 3) For first line get starting location of each field.  field_list is a list of dictionary as
+lines = status_file.readlines()
+
+# For first line get starting location of each field.  field_list is a list of dictionary as
 #    [ column_name: start]
 heading = lines[0]
 fields = {'CONTAINER ID':0, 'IMAGE':0, 'COMMAND':0, 'CREATED':0, 'STATUS':0, 'PORTS':0, 'NAMES':0}
@@ -42,3 +50,16 @@ for line in lines[1:]:
 
 exit(container + ' not found')
 
+# context-server-pcc_qa
+# context-nginx
+# clearcase
+# teamcenter
+# context-redis
+# context-server-pc_qa_prod
+# context-myadmin
+# context-mysql
+
+# To Do:
+#   - Create and document de08u2008 cron jobs to create docker ps, docker check time and system ps files
+#   - Update python program to read all of these files and return based on query type
+#   - Package up the stuff that goes on head CSDM server.  cron scripts, plugins
